0e64c7f0249d8dde18f650d3888b9466c09e564d
Vadym Brodetskyi <amazingtr@gmail.com>
2021-11-23 01:02:38 +0200

    add all files

--- js/partners.js
+++ js/partners.js
@@ -1,11 +1,32 @@
-const array = [11, 34, 67, 798, 45]
+fetch('./db/partners.json')
+  .then((response) => response.json())
+    .then((data) => {console.log(data);})
 
-array.forEach((elem) => {
-  console.log(elem);
-  console.log(index);
-  console.log(array);
-} )
+
+
+// const array = [11, 32, 432, 123, 765]
+
+//   array.forEach((elem, index) => {
+//     if (index < 2){
+//       console.log(elem);
+//     } else if (index === 3) {
+//       console.log(elem)
+//     }
+//   })
+
+// const array = [11, 34, 67, 798, 45]
+
+// array.forEach((elem) => {
+//   console.log(elem);
+//   console.log(index);
+//   console.log(array);
+// } )
 
 // for (let i = 0; i < array.length; i++) {
 //   console.log(array[i]);
+// }
+
+// const arr = [15, 20, 25, 45, 48]
+// for (let j = 0; j < arr.length; j++) {
+//   console.log(arr[j]);
 // }
