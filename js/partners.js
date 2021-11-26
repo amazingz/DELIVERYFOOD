9489d68f4751df71bbadad349de61afe375b2c8e
Vadym Brodetskyi <amazingtr@gmail.com>
2021-11-26 02:35:59 +0200

    createElement, classList, innerHTML and more

--- js/partners.js
+++ js/partners.js
@@ -1,5 +1,35 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+// cardsRestaurants.classList.add('testClass')
+// cardsRestaurants.classList.remove('testClass')
+// cardsRestaurants.classList.toggle('testClass')
+// console.dir(cardsRestaurants);
+
 const renderItems = (data) => {
-  console.log(data);
+  data.forEach(({image, kitchen, name, price, products, stars, time_of_delivery})  => {
+    const a = document.createElement('a')
+      console.log(image, kitchen, name, price, products, stars, time_of_delivery);
+    //card card-restaurant
+      a.setAttribute('href', 'restaurant.html')
+      a.classList.add('card')
+      a.classList.add('card-restaurant')
+      a.innerHTML = `
+          <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
+          <div class="card-text">
+            <div class="card-heading">
+              <h3 class="card-title">Пицца плюс</h3>
+              <span class="card-tag tag">50 мин</span>
+            </div>
+            <div class="card-info">
+              <div class="rating">
+                4.5
+              </div>
+              <div class="price">От 900 ₽</div>
+              <div class="category">Пицца</div>
+            </div>
+          </div>
+      `
+      console.log(a);
+  });
 }
 
 fetch('https://food-delivery-3c5d5-default-rtdb.europe-west1.firebasedatabase.app/db/partners.json')
@@ -9,25 +39,30 @@ fetch('https://food-delivery-3c5d5-default-rtdb.europe-west1.firebasedatabase.ap
         console.log(error);
       })
 
-fetch('./db/food-band.json')
-  .then((response) => response.json())
-    .then((data) => {console.log(data)} )
+  // fetch('./db/pizza-burger.json')
+  //   .then((response) => response.json())
+  //     .then((data) => {renderItems(data)})
+  //       .catch((error) => {console.log(error)})
 
-fetch('./db/pizza-burger.json')
-.then((response) => response.json())
-  .then((data) => {console.log(data)})
+// fetch('./db/food-band.json')
+//   .then((response) => response.json())
+//     .then((data) => {console.log(data)} )
 
-fetch('./db/pizza-plus.json')
-  .then((response) => response.json())
-    .then((data) => {console.log(data)})
+// fetch('./db/pizza-burger.json')
+// .then((response) => response.json())
+//   .then((data) => {console.log(data)})
+
+// fetch('./db/pizza-plus.json')
+//   .then((response) => response.json())
+//     .then((data) => {console.log(data)})
     
- fetch('./db/tanuki.json')
-  .then((response) => response.json())
-    .then((data) => {console.log(data)})
+//  fetch('./db/tanuki.json')
+//   .then((response) => response.json())
+//     .then((data) => {console.log(data)})
 
-fetch('./db/gusi-lebedi.json')
-  .then((response) => response.json())
-    .then((data) => {console.log(data);})
+// fetch('./db/gusi-lebedi.json')
+//   .then((response) => response.json())
+//     .then((data) => {console.log(data);})
 
 
 // const array = [11, 32, 432, 123, 765]
